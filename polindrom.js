78582c36824e30e19f3352ac9cd70d513047dae5
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-08-22 21:57:26 +0600

    polindrom problem solve

---
+++ polindrom.js
@@ -0,0 +1,14 @@
+function isPalindrome(str) {
+    // স্ট্রিংকে ছোট হাতের অক্ষরে রূপান্তর করা
+    str = str.toLowerCase();
+    
+    // স্ট্রিংয়ের উল্টা অংশ তৈরি করা
+    let reversedStr = str.split('').reverse().join('');
+    
+    // মূল স্ট্রিং এবং উল্টা স্ট্রিং তুলনা করা
+    return str === reversedStr;
+}
+
+// উদাহরণ:
+console.log(isPalindrome("Madam")); // true
+console.log(isPalindrome("Hello")); // false
