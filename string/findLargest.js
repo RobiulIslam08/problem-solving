474f92e53cecb1fbd09b35b30c8154165a4c5e81
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-09-09 21:41:54 +0600

    practise

--- string/findLargest.js
+++ string/findLargest.js
@@ -1,4 +1,9 @@
-function findLargest(a, b) {
-	return a > b ? a : b;
-  }
-  console.log(findLargest(10, 20));
+// function findLargest(a, b) {
+// 	return a > b ? a : b;
+//   }
+//   console.log(findLargest(10, 20));
+const findLargest = (...num)=>{
+  return Math.max(...num)
+
+}
+console.log(findLargest(1,35,64,66,5,343))
