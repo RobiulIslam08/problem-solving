ca6c8ed7b867c07fdd97f0372ad77b81bf40b6d7
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-10-11 14:17:08 +0600

    capitalize word practise

--- string/capitalizeWords.js
+++ string/capitalizeWords.js
@@ -5,3 +5,15 @@
 // // উদাহরণ:
 // console.log(capitalizeWords("javaScript is awesome")); // "JavaScript Is Awesome"
 // console.log(capitalizeWords("hello world")); // "Hello World"
+
+// practise
+// const capitalizeWords = (str) =>{
+// 	return str.split(' ').map((word)=> word.charAt(0).toUppercase())
+// }
+// console.log(capitalizeWords('Robi'))
+const capitalizeWords = (str) => {
+	return str.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('');
+  }
+  
+  console.log(capitalizeWords('robi'));
+  
