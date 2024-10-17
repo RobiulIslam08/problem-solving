7d3b6c94e19be866b8f4d7873913945e694f91aa
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-10-17 20:06:38 +0600

    filter even number

--- string/filterEvenNumbers.js
+++ string/filterEvenNumbers.js
@@ -1,5 +1,12 @@
-const filterEvenNumbers = (arry)=>{
-	return arry.filter(num => num % 2 ===0)
+// const filterEvenNumbers = (arry)=>{
+// 	return arry.filter(num => num % 2 ===0)
+// }
+// const number = [1,22,52,51,2,33,10,15,18]
+// const result = filterEvenNumbers(number)
+// console.log(result)
+
+const filterEvenNumbers = (arry) =>{
+	return arry.filter(num => num % 2 ==0)
 }
 const number = [1,22,52,51,2,33,10,15,18]
 const result = filterEvenNumbers(number)
