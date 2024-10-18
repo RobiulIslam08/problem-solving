4b8e36fad4abd618b1dc9d892378fdcff6f41210
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-10-18 18:14:50 +0600

    arry sum

--- Array/sumArray.js
+++ Array/sumArray.js
@@ -1,4 +1,8 @@
-const sumArray = (arr)=>{
-	return arr.reduce((sum, num)=> sum +num,0)
+// const sumArray = (arr)=>{
+// 	return arr.reduce((sum, num)=> sum +num,0)
+// }
+// console.log(sumArray([1,4,5,5]))
+const arrySum = (arr) =>{
+	return arr.reduce((sum, num)=> sum + num, 0)
 }
-console.log(sumArray([1,4,5,5]))
+console.log(arrySum([1,2,3]))
