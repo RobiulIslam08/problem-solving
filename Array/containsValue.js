a0fac3ed1b87b7ca6890c9bf6b2a22b81f7499ac
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-12-29 00:24:26 +0600

    contains value

--- Array/containsValue.js
+++ Array/containsValue.js
@@ -1,3 +1,7 @@
+// function containsValue(arr, value) {
+//     return arr.includes(value);
+// }
+// console.log(containsValue([1, 2, 3, 4], 3)); // true
 function containsValue(arr, value) {
     return arr.includes(value);
 }
