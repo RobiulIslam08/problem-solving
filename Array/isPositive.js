dbfa8fe0fe07c2c4c1e21908de991fe0415492fb
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-04 21:06:33 +0600

    possitive number check

--- Array/isPositive.js
+++ Array/isPositive.js
@@ -1,3 +1,6 @@
+// const numbers = [1, 2, 3, 4];
+// const allPositive = numbers.every(num => num > 0);
+// console.log(allPositive); // Output: true
 const numbers = [1, 2, 3, 4];
 const allPositive = numbers.every(num => num > 0);
-console.log(allPositive); // Output: true
+console.log(allPositive);
