6874573c6a531edf441243aeb9c762b6d6b335d4
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-11 18:46:50 +0600

    check largest number

--- Array/isPositive.js
+++ Array/isPositive.js
@@ -1,6 +1,10 @@
 // const numbers = [1, 2, 3, 4];
 // const allPositive = numbers.every(num => num > 0);
 // console.log(allPositive); // Output: true
-const numbers = [1, 2, 3, 4];
-const allPositive = numbers.every(num => num > 0);
-console.log(allPositive);
+// const numbers = [1, 2, 3, 4];
+// const allPositive = numbers.every(num => num > 0);
+// console.log(allPositive);
+const largestNumber = (arr) => {
+	return [...new Set(arr)]
+}
+console.log(largestNumber([4,4,4,4,4, 55, 555, 3333, 23, 55343, 43]));
