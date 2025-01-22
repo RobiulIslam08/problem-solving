f023af78c0446b8572e21ee7fa08caf240d4b1da
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-22 22:48:37 +0600

    even number

--- loop/even_number.js
+++ loop/even_number.js
@@ -1,3 +1,8 @@
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
 for (let i = 1; i <= 50; i++) {
     if (i % 2 === 0) {
         console.log(i);
