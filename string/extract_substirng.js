137ff3b43cb963fa8eda6e2e6871104cfd3f4d6c
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-24 21:22:09 +0600

    extract_substring

--- string/extract_substirng.js
+++ string/extract_substirng.js
@@ -1,3 +1,6 @@
+// const str = "Welcome to JavaScript!";
+// const part = str.slice(11, 21); // Extracts "JavaScript"
+// console.log(part);
 const str = "Welcome to JavaScript!";
 const part = str.slice(11, 21); // Extracts "JavaScript"
 console.log(part);
