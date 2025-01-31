6311493199a9b407013d0ee7c8e2772a802064d3
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-31 21:33:54 +0600

    count vowel

--- loop/count_vowel.js
+++ loop/count_vowel.js
@@ -1,3 +1,12 @@
+// const str = "hello world";
+// const vowels = 'aeiou';
+// let count = 0;
+// for(let char of str.toLowerCase()){
+// 	if(vowels.includes(char)){
+// 		count ++
+// 	}
+// }
+// console.log(count)
 const str = "hello world";
 const vowels = 'aeiou';
 let count = 0;
