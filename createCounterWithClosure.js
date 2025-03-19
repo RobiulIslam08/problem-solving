89b82204a58f21a9d896880fae3499c5e2c45e0d
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-03-19 21:56:17 +0600

    event loop

--- createCounterWithClosure.js
+++ createCounterWithClosure.js
@@ -1,10 +1,16 @@
 function createCounter() {
-    let count = 0;
-    return function() {
-        count++;
-        return count;
+    let count = 0; // এই ভেরিয়েবলটি শুধুমাত্র এই ফাংশনের ভিতরেই অ্যাক্সেসযোগ্য
+
+    return function() { // এই ফাংশনটি একটি ক্লোজার
+        count++; // প্রতিবার কল করলে কাউন্ট বাড়ে
+        return count; // বর্তমান কাউন্ট রিটার্ন করে
     };
 }
+
+// কাউন্টার তৈরি করা
 const counter = createCounter();
+
+// কাউন্টার ব্যবহার করা
 console.log(counter()); // 1
 console.log(counter()); // 2
+console.log(counter()); // 3
