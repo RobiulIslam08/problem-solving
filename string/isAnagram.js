f7b5262974127ed7e3ef1912392f1b23678f62bb
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-03-22 22:30:27 +0600

    practise angram

--- string/isAnagram.js
+++ string/isAnagram.js
@@ -1,3 +1,9 @@
+// function isAnagram(str1, str2) {
+// 	return str1.split('').sort().join('') === str2.split('').sort().join('');
+//   }
+  
+//   console.log(isAnagram("listen", "silent")); // true
+//   console.log(isAnagram("hello", "world"));   // false
 function isAnagram(str1, str2) {
 	return str1.split('').sort().join('') === str2.split('').sort().join('');
   }
